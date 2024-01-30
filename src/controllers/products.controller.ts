7eaa1dbfa7be071e3ec82e21fba459e0463c815c
Eduardo <rcchristiane@gmail.com>
2024-01-29 18:32:16 -0600

    clase de métodos http

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -7,17 +16,37 @@ export class ProductsController {
     @Query('limit') limit = 100,
     @Query('offset') offset = 0,
     @Query('brand') brand: string,
-  ): string {
-    return `products: limit => ${limit}, offset => ${offset}, brand => ${brand}`;
+  ): object {
+    console.log('limit', limit);
+    return {
+      limit,
+      offset,
+      brand,
+    };
   }
 
   @Get('/filter')
-  produtFilter(): string {
-    return `yo soy un filter`;
+  filter(): object {
+    return { message: 'yo soy un filter' };
   }
 
   @Get('/:productId')
-  produt(@Param('productId') productId: string): string {
-    return `product ${productId}`;
+  get(@Param('productId') productId: string): object {
+    return { message: `product ${productId}` };
+  }
+
+  @Post()
+  create(@Body() payload: any): object {
+    return { message: 'yo soy un post', payload };
+  }
+
+  @Put('/:id')
+  update(@Param('id') id: number, @Body() payload: any): object {
+    return { id, payload };
+  }
+
+  @Delete('/:id')
+  delete(@Param('id') id: number): object {
+    return { id };
   }
 }
